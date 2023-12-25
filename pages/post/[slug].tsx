ac791570137a85863b3fd8a38cc82adef8da73a8
Abhinav Raghunandan <raghunandan.abhinav@gmail.com>
2023-12-25 14:28:02 +0530

    last commit

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -3,9 +3,10 @@ import { useRouter } from 'next/router';
 
 import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm, Loader } from '../../components';
 import { getPosts, getPostDetails } from '../../services';
-// import { AdjacentPosts } from '../../sections';
+import { AdjacentPosts } from '../../sections';
 
-const PostDetails = ({ post }) => {
+
+const PostDetails = ({ post }: any) => {
   const router = useRouter();
 
   if (router.isFallback) {
@@ -19,7 +20,7 @@ const PostDetails = ({ post }) => {
           <div className="col-span-1 lg:col-span-8">
             <PostDetail post={post} />
             <Author author={post.author} />
-            {/* <AdjacentPosts slug={post.slug} createdAt={post.createdAt} /> */}
+            <AdjacentPosts slug={post.slug} createdAt={post.createdAt} />
             <CommentsForm slug={post.slug} />
             <Comments slug={post.slug} />
           </div>
@@ -52,6 +53,6 @@ export async function getStaticPaths() {
   const posts = await getPosts();
   return {
     paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
-    fallback: false,
+    fallback: true,
   };
 }
